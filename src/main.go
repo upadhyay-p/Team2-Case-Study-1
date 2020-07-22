6ce4525b90a2713f8cb59093a04bc764c583acf6
Shashi Jangra <shashijangra@live.com>
2020-07-23 00:18:53 +0530

    added commandnline args to main file

--- src/main.go
+++ src/main.go
@@ -1,7 +1,15 @@
 package main
 
-import "CSV2JSON"
+import (
+	"CSV2JSON"
+	"os"
+)
 
 func main() {
-	CSV2JSON.INIT("../assets/orders.csv")
+	args := os.Args[1:]
+	if len(args) == 1 {
+		CSV2JSON.INIT(args[0])
+	} else {
+		println("Please Give File name as an Argument!")
+	}
 }
